6b5c8ee3276fc67e6a1bd0ec97a4b3ceb0c02ae8
akshay43215 <akshaykrishna24996@gmail.com>
2023-04-19 20:01:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './Components/Counter/Counter'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const addfunction =()=>{
+    // console.log('addfunc app')
+    (count<10) ? setCount(prev=> prev+1): alert('Increment is limited upto 10')
+  } 
+  const subfunction =()=>{
+  //  console.log('subfunc app')
+    (count>0)?setCount(prev=>prev-1):alert('Negative Value is not possible')
+
+  } 
+
+  return (
+    <div className="App">
+       
+      <h1>Counter App</h1>
+      <div className="card">
+        <Counter count={count} increment={addfunction} decrement={subfunction}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
